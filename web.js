2f4d75ee89784fd2eb99d6ae8a1090dcd229ee50
dwidemann <dwidemann@gmail.com>
2013-07-10 05:43:50 +0000

    added index and web.js reads from it

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
-var express = require('express');
-
+var exppess = require('express');
+var fs = require('fs');
+var fn = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var indexString = fs.readFileSync(fn).toString().split('\n');
+    //response.send('Hello World 2!');
+    response.send(indexString[0]);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
